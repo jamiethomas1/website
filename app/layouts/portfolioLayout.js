4d1c5db26f4c58962071f4eca662d49c4c30aaf4
cogsincogs <jamiewthomas13@gmail.com>
2024-02-22 00:16:31 +0000

    Add logo

--- app/layouts/portfolioLayout.js
+++ app/layouts/portfolioLayout.js
@@ -18,8 +18,8 @@ export default function PortfolioLayout() {
         <>
             <div className="relative min-h-screen min-w-full flex flex-col items-center">
                 <div className={styles.background} />
-                <div id="navbar" className="p-6 min-w-full flex justify-around items-center">
-                    <h1>Jamie Thomas</h1>
+                <div id="navbar" className="p-6 min-w-full max-h-28 flex justify-around items-center">
+                    <img className="logo" src="/logo-test.png" alt="Jamie Thomas" style={{"max-width": "10%"}} />
                     <ul id="links" className="flex justify-around space-x-10">
                         <li className={styles.selected}>Home</li>
                         <li onClick={aboutScroll}><a href="#about">About</a></li>
